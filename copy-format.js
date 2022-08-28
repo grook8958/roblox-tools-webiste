9cb825c5d307283db11d03639db8f0380646a807
grook8958 <78109323+grook8958@users.noreply.github.com>
2022-08-28 17:00:04 +0100

    Update copy-format.js

--- copy-format.js
+++ copy-format.js
@@ -4,4 +4,10 @@ const copyBtn = document.getElementById('copy-format-btn');
 
 copyBtn.addEventListener('click', async () => {
   //WIP
+  const username = getText("roblox-info-username")
+  const displayName = getText("roblox-info-displayname")
+  const pastUsernames = getText("roblox-info-pastusernames")
+  const id = getText("roblox-info-id")
+  const groups = getText("roblox-info-groups")
+  const rank = getText("roblox-info-usmcrank")
 });
