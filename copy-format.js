55e0a012b4287497667a0489b38f448c9eaf7a09
grook8958 <78109323+grook8958@users.noreply.github.com>
2022-08-29 21:51:08 +0200

    Fixed + Tested copy format feature

--- copy-format.js
+++ copy-format.js
@@ -1,18 +1,19 @@
-const getText = (id) => document.getElementById(id);
+const getText = (id) => document.getElementById(id).innerHTML;
+const removeTitle = (string) => {
+  let newString = string.replace(/(\s)+/gm, '');
+  newString = newString.match(/(?<=\:)(.+)/gm);
+  return newString[0];
+}
 
 const copyBtn = document.getElementById('copy-format-btn');
 
 copyBtn.addEventListener('click', async () => {
   const regex = /\[(.*?)\]/gm
-  const getRawRank = rank => rank.match(regex).replace('[','').replace(']','');
-  
+  const getRawRank = rank => rank.match(regex)[0].replace('[','').replace(']','');
   const username = getText("roblox-info-username")
-  const displayName = getText("roblox-info-displayname")
-  const pastUsernames = getText("roblox-info-pastusernames")
   const id = getText("roblox-info-id")
   const groups = getText("roblox-info-groups")
   const rank = getText("roblox-info-usmcrank")
-  
-  const format = `Ticket:\nAgent:\nSuspect: ${username}\nSuspect's Rank: ${getRawRank(rank)}\nSuspect's Regiment: ${groups}\nSuspect's Roblox ID: ${id}\nOffence:\nPunishment:\nApproved by:`
-  navigator.clipboard.navigator.wrtiteText(format);
+  const format = `Ticket:\nAgent:\nSuspect: ${removeTitle(username)}\nSuspect's Rank: ${getRawRank(rank)}\nSuspect's Regiment: ${removeTitle(groups)}\nSuspect's Roblox ID: ${removeTitle(id)}\nOffence:\nPunishment:\nApproved by:`
+  navigator.clipboard.writeText(format);
 });
